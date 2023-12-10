eeb5bdc5d6c969ef9aae911cef17c80328b5181e
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-10 00:58:22 -0800

    feat(mysql): user - GET, UPDATE, DELETE

--- app/src/main/java/todolist/database/mysql/SqlOperation.java
+++ app/src/main/java/todolist/database/mysql/SqlOperation.java
@@ -7,7 +7,10 @@ public enum SqlOperation {
     RETRIEVE_USER_TOKEN_FROM_TOKEN("SELECT * FROM Token WHERE jwt_value = ?;"),
     CREATE_USER_TOKEN("INSERT INTO Token (jwt_value, expiration_date, user, is_activate) VALUES (?, ?, ?, ?);"),
     DELETE_USER_TOKEN("DELETE FROM Token WHERE user = ?;"),
-    UPDATE_USER_TOKEN("UPDATE Token SET jwt_value = ?, expiration_date = ?, is_activate = ? WHERE token_id = ?;");
+    UPDATE_USER_TOKEN("UPDATE Token SET jwt_value = ?, expiration_date = ?, is_activate = ? WHERE token_id = ?;"),
+    RETRIEVE_USER_FROM_TOKEN("SELECT user.user_id, user.firstname, user.lastname, user.email, user.password FROM Token token JOIN User user ON token.user = user.user_id WHERE token.jwt_value = ?;"),
+    UPDATE_USER("UPDATE Token token JOIN User user ON token.user = user.user_id SET user.firstname = ?, user.lastname = ? WHERE token.jwt_value = ?;"),
+    DELETE_USER("DELETE user, token FROM Token token JOIN User user ON token.user = user.user_id WHERE token.jwt_value = ?;");
 
     private final String sqlTemplate;
 
